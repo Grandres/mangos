e007657d224a1364dc4b2a2d116c1248ecce4469
Emtec <kulczycki.k@gmail.com>
2010-07-26 19:10:41 +0200

    Outdoor pvp start

--- src/game/Vehicle.cpp
+++ src/game/Vehicle.cpp
@@ -22,7 +22,10 @@
 #include "Vehicle.h"
 #include "Unit.h"
 #include "Util.h"
-
+#include "WorldPacket.h"
+#include "InstanceData.h"
+#include "ZoneScript.h"
+ 
 Vehicle::Vehicle() : Creature(CREATURE_SUBTYPE_VEHICLE), m_vehicleId(0)
 {
     m_updateFlag = (UPDATEFLAG_LIVING | UPDATEFLAG_HAS_POSITION | UPDATEFLAG_VEHICLE);
